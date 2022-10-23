8e5064265bc75b284ed9bc4b516a7e4bf33e2376
EPourmo <94918200+EPourmo@users.noreply.github.com>
2022-10-23 15:37:59 +0200

    change protected route path

--- frontend/code/src/routing/ProtectedRoute.jsx
+++ frontend/code/src/routing/ProtectedRoute.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 
 import { useSelector } from "react-redux";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, Navigate } from "react-router-dom";
 
 const ProtectedRoute = () => {
   const { isConnected } = useSelector((state) => state.user);
   if (!isConnected) {
-    return (
-      <div className="unauthorized">
-        <h1>Unauthorized :(</h1>
-        <span>
-          <Link to="/login">Login</Link> to gain access
-        </span>
-      </div>
-    );
+    return <Navigate to="/login" replace />;
   }
 
   return <Outlet />;
